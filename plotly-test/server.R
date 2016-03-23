6f258ba05634591b36981d731cee750498d35fca
Brian Stamper <brian.stamper@chrr.osu.edu>
2016-03-23 11:54:58 -0400

    Trying to get global.R loaded..

--- plotly-test/server.R
+++ plotly-test/server.R
@@ -1,12 +1,8 @@
 
 library(plotly)
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
+
+source("global.R")
 
 shinyServer(function(input, output) {
   
